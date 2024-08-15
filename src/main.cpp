1d1af0b6cefa44ca2cf4334f380d26972cf64625
TsilaAllaoui <tsila.allaoui@gmail.com>
2024-08-15 19:15:29 +0300

    Update args

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
         .action([](const std::string &value)
                 { return value; });
 
-    program.add_argument("-b", "--usebios")
-        .help("boot with BIOS (true or false)")
-        .default_value(true)
+    program.add_argument("-s", "--skip_bios")
+        .help("skip BIOS on boot")
+        .default_value(false)
         .implicit_value(true);
 
     program.add_argument("-d", "--debug")
-        .help("boot in debug mode (true or false)")
+        .help("boot in debug mode")
         .default_value(false)
         .implicit_value(true);
 
@@ -29,25 +29,25 @@ int main(int argc, char *argv[])
     {
         program.parse_args(argc, argv);
         std::string romFile = std::filesystem::path(program.get<std::string>("--rom")).make_preferred().string();
-        bool useBios = program.get<bool>("--usebios");
+        bool skipBios = program.get<bool>("--skip_bios");
         bool debugMode = program.get<bool>("--debug");
 
         auto logger = gasyboy::utils::Logger::getInstance();
         logger->log(gasyboy::utils::Logger::LogType::FUNCTIONAL,
                     "Rom file: " + romFile +
                         "\n\t - Use BIOS: " +
-                        (useBios ? "true" : "false") +
+                        (!skipBios ? "true" : "false") +
                         "\n\t - Debug Mode: " +
                         (debugMode ? "true" : "false"));
 
-        gasyboy::GameBoy(romFile, useBios, debugMode).boot();
+        gasyboy::GameBoy(romFile, skipBios, debugMode).boot();
     }
     catch (const std::runtime_error &err)
     {
         std::cout << err.what() << "\n";
         std::cout << "usage: gasyboy [-r | --rom rom_file_path] [--usebios]\n"
                   << "\t-r | --rom : the path to the rom file to load\n"
-                  << "\t-b | --usebios : use BIOS (default: true)\n"
+                  << "\t-s | --skip_bios : skip BIOS on boot (default: false)\n"
                   << "\t-d | --debug : boot in debug mode (default: false)\n";
         return 1;
     }
