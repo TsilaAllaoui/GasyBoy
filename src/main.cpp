32a35b15b34bb988181828835426f92249476483
TsilaAllaoui <tsila.allaoui@gmail.com>
2024-08-15 19:08:39 +0300

    Updated arguments

--- src/main.cpp
+++ src/main.cpp
@@ -20,24 +20,35 @@ int main(int argc, char *argv[])
         .default_value(true)
         .implicit_value(true);
 
+    program.add_argument("-d", "--debug")
+        .help("boot in debug mode (true or false)")
+        .default_value(false)
+        .implicit_value(true);
+
     try
     {
         program.parse_args(argc, argv);
         std::string romFile = std::filesystem::path(program.get<std::string>("--rom")).make_preferred().string();
         bool useBios = program.get<bool>("--usebios");
+        bool debugMode = program.get<bool>("--debug");
 
         auto logger = gasyboy::utils::Logger::getInstance();
         logger->log(gasyboy::utils::Logger::LogType::FUNCTIONAL,
-                    "Rom file: " + romFile + ", Use BIOS: " + (useBios ? "true" : "false"));
+                    "Rom file: " + romFile +
+                        "\n\t - Use BIOS: " +
+                        (useBios ? "true" : "false") +
+                        "\n\t - Debug Mode: " +
+                        (debugMode ? "true" : "false"));
 
-        gasyboy::GameBoy(romFile, useBios).boot();
+        gasyboy::GameBoy(romFile, useBios, debugMode).boot();
     }
     catch (const std::runtime_error &err)
     {
         std::cout << err.what() << "\n";
         std::cout << "usage: gasyboy [-r | --rom rom_file_path] [--usebios]\n"
                   << "\t-r | --rom : the path to the rom file to load\n"
-                  << "\t-b | --usebios : use BIOS (default: true)\n";
+                  << "\t-b | --usebios : use BIOS (default: true)\n"
+                  << "\t-d | --debug : boot in debug mode (default: false)\n";
         return 1;
     }
 
